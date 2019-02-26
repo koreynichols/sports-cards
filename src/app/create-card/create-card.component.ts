725fc3706f321b0d77a4cfe303ea518775ed3b35
Korey Nichols <knichols116@hotmail.com>
2019-02-25 20:31:54 -0800

    card image preview added

--- src/app/create-card/create-card.component.ts
+++ src/app/create-card/create-card.component.ts
@@ -9,6 +9,30 @@ export class CreateCardComponent implements OnInit {
 
   constructor() { }
 
+  selectedFile: File;
+  public imagePath;
+  imgURL: any;
+  public message: string;
+
+  preview(files) {
+    if (files.length === 0) {
+      return;
+    }
+ 
+    const mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      this.message = 'Only images are supported.';
+      return;
+    }
+ 
+    const reader = new FileReader();
+    this.imagePath = files;
+    reader.readAsDataURL(files[0]);
+    reader.onload = (_event) => {
+      this.imgURL = reader.result;
+    };
+
+  }
   ngOnInit() {
   }
 
